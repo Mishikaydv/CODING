bed4bc4be7dd7596706546517cc3dcb0a26d426c
Mishika Yadav <mishikaydv11@gmail.com>
2026-02-20 00:48:12 +0530

    learnt operators & conditional statement

--- javascript/first.js
+++ javascript/first.js
@@ -74,13 +74,114 @@
 // console.log("a<=b",a<=b); //less than or equal to
 
 
-//logical operators
+//logical &&operators
 
-let a =8;
-let b =6;
-console.log("a=",a,"& b=",b);
+// let a =8;
+// let b =6;
+// console.log("a=",a,"& b=",b);
 
-let condition1 = a>b;
-let condition2=a<b;
-console.log("condition1 && condition2 =",condition1 && condition2);  
+// let condition1 = a>b;
+// let condition2=a<b;
+// console.log("condition1 && condition2 =",condition1 && condition2);  
+
+//logical OR|| operators
+
+// let a =8;
+// let b =6;
+// console.log("a=",a,"& b=",b);
+
+// let condition1=a<b;
+// let condition2=a>b;
+// console.log("condition1 || condition2=",condition1||condition2);
+
+//logical NOT! operator
+
+// let a =8;
+// let b =6;
+// console.log("!a===b=",!a===b);
+
+//CONDITIONAL STATEMENTS
+
+// let age=16;
+
+// if(age>=18){
+//     console.log("you can vote")
+// }
+//    else(age<18)
+//     { console.log("you cannot vote")}
+    
+// let mode="dark";
+// let color;
+// if(mode==="dark"){
+//     color="black"
+// }
+// console.log(color);
+
+// let a=7;
+
+// if(a%2===0){
+//     console.log(a,"is even");
+// }
+// else{
+//     console.log(a,"is odd");}
+
+    //syntax -> rules
+
+    //else if statement
+
+    // let mode="dark";
+    // let color;
+
+//     if(mode==="dark"){
+//         color="black"
+//     }else if(mode==="blue"){
+//         color="blue"
+//     }else if(mode==="pink"){
+//         color="pink"
+//     }else{
+//         color="white"
+//     }
+// console.log(color);
+
+// ternary operators
+
+// let age=17;
+// let result=age>=18?"adult": "not adult";
+// console.log(result);
+
+//#practice question
+//1.
+
+// let number=prompt("give number");
+
+// if(number%5===0){
+//     console.log(number,"is a multiple of 5");
+// }else{console.log(number,"is not a multiple of 5");}
+
+//2.
+ 
+// let score=prompt("enter your score");
+// let grade;
+
+// if(score >=90 && score <=100){
+//     grade="A";
+// }else if(score >=70 && score<=89){
+//     grade="B";
+// }else if(score >=60 && score<=69){
+//     grade="C";
+// }else if(score >=50 && score<=59){
+//     grade="D";
+// }else if(score >=0 && score<=49){ 
+//     grade="F";
+// }
+
+// console.log("according to your score your grade was:",grade)
+   
+
+
+
+
+
+
+
 
