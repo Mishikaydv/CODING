bcafdecdf419be2d7476aa377259563d9597c28e
Mishika Yadav <mishikaydv11@gmail.com>
2026-02-19 19:19:02 +0530

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,86 @@
+//operators in js
+//1.Arithmetic operators
+//let a =6;
+//let b =4;
+//console.log("a=6 & b=4");
+//console.log("a+b=",a+b); //addition
+//console.log("a-b=",a-b); //subtraction
+//console.log("a*b=",a*b);    //multiplication
+//console.log("a/b=",a/b);    //division
+//console.log("a%b=",a%b);    //modulus
+//console.log("a**b=",a**b);  //exponentiation
+
+
+//we have two unary operators in js
+//1.increment operator
+
+//let a =6;
+//let b =4;
+//console.log("a=",a,"& b=",b);
+//a++;
+//console.log("a++=",a);
+//console.log("++a=",a);
+
+//2.decrement operator
+
+//a--;
+//console.log("a--=",a);
+//console.log("--a=",a);
+//++a; //pre increment
+//--a; //pre decrement
+//a++; //post increment
+//a--; //post decrement
+
+//assignment operators
+
+// let a =8;
+// let b =2;
+// console.log("a=",a,"& b=",b);
+// a +=b;
+// console.log("a+=b=",a);
+
+// a -=b;
+// console.log("a-=b=",a);
+
+// a *=b;
+// console.log("a*=b=",a);
+
+// a /=b;
+// console.log("a/=b=",a);
+
+// a %=b;
+// console.log("a%=b=",a);
+
+//comparison operators
+
+// let a =8; 
+// let b =2;
+// console.log("a=",a,"& b=",b);
+
+// console.log("a!=b",a!=b); //notequal to
+
+// console.log("a==b",a==b); //equal to
+
+// console.log("a===b",a===b); //strict checking
+
+// console.log("a!==b",a!==b);
+
+// console.log("a>b",a>b); //greater than
+
+// console.log("a<b",a<b); //less than
+
+// console.log("a>=b",a>=b); //greater than or equal to
+
+// console.log("a<=b",a<=b); //less than or equal to
+
+
+//logical operators
+
+let a =8;
+let b =6;
+console.log("a=",a,"& b=",b);
+
+let condition1 = a>b;
+let condition2=a<b;
+console.log("condition1 && condition2 =",condition1 && condition2);  
+
