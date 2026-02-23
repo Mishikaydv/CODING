2ced361251fc953ea2e6bd19bb550bd2e8f60ba7
Mishika Yadav <mishikaydv11@gmail.com>
2026-02-24 01:13:00 +0530

    PRACTICE QUESTIONS OF FUNCTIONS IN JS.

--- javascript/first.js
+++ javascript/first.js
@@ -491,3 +491,26 @@
 
 // const printHello=()=> console.log("hello");
 
+//# PRACTICE QUESTIONS
+1
+//1.
+
+// function countVowels(str){
+//     let count=0
+//    for(const char of str) {
+//     if(char==="a"|| char==="e"|| char==="i"||char==="o"||char==="u"){
+//         count++;
+//     }
+// }
+// return count;
+// }
+// const countVow=(str)=>{
+//         let count=0
+//    for(const char of str) {
+//     if(char==="a"|| char==="e"|| char==="i"||char==="o"||char==="u"){
+//         count++;
+//     }
+// }
+// return count;
+
+// };
