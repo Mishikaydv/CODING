8c54e65549fc053fcb187a8cd6da5d9cc4944f52
Mishika Yadav <mishikaydv11@gmail.com>
2026-02-25 03:55:17 +0530

    DOM completed

--- javascript/first.js
+++ javascript/first.js
@@ -626,3 +626,33 @@
 // let h2=document.querySelector("h2");
 // console.dir(h2.innerText);
 // h2.innerText= h2.innerText+"from mishika yadav";
+
+//DOM ATTRIBUTES//
+
+// let div=document.querySelector("div");
+// console.log(div.setAttribute("id","newid"));
+
+// let div=document.querySelector("div");
+// div.style.backgroundColor="pink";
+// div.style.fontSize="26px";
+
+// let newBtn=document.createElement("button");
+// newBtn.innerText="click me!";
+// console.log(newBtn);
+
+// let div=document.querySelector("div");
+// div.append(newBtn);
+
+//# PRACTICE QUESTIONS //
+//1.
+
+
+// let newBtn=document.createElement("button");
+// newBtn.innerText="click me!";
+// newBtn.style.color="white";
+// newBtn.style.backgroundColor="red";
+
+// document.querySelector("body").prepend(newBtn);
+
+
+
