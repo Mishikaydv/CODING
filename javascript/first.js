780e076d63e4b31d93c163ee6eea49d42c7d15e0
Mishika Yadav <mishikaydv11@gmail.com>
2026-02-25 23:06:28 +0530

    EVENTS IN JS

--- javascript/first.js
+++ javascript/first.js
@@ -654,5 +654,55 @@
 
 // document.querySelector("body").prepend(newBtn);
 
+//## EVENTS IN JS //
+//# EVENT OBJECT
 
+// let btn1=document.querySelector("#btn1");
 
+// btn1.onclick=(e)=>{
+//     console.log(e);
+//     console.log(e.type);
+//      console.log(e.target);
+// };
+
+// let div=document.querySelector("div");
+// div.onmouseover=()=>{
+//     console.log("you are inside div");
+// };
+
+//# EVENT LISTENERS//
+
+// btn1.addEventListener("click",(evt)=>{
+//     console.log("button1 was clicked - handler1");
+// });
+
+// btn1.addEventListener("click",()=>{
+//     console.log("button1 was clicked-handler2");
+// });
+
+// const handler3=()=>{
+// console.log("button1 was clicked-handler3");
+// };
+// btn1.addEventListener("click",handler3);
+
+// btn1.addEventListener("click",()=>{
+//     console.log("button1 was clicked-handler4");
+// });
+// btn1.removeEventListener("click",handler3);
+
+//# PRACTICE QUESTIONS//
+
+// let modeBtn=document.querySelector("#mode");
+// let currMode="light";
+
+// modeBtn.addEventListener("click",()=>{
+// if(currMode==="light"){
+//     currMode="dark";
+//     document.querySelector("body").style.backgroundColor="black";
+// }else{
+//     currMode="light";
+//  document.querySelector("body").style.backgroundColor="white";
+// }
+
+// console.log(currMode);
+// });
